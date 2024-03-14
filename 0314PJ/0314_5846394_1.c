3d0b6c9e714efa0a690e351fca8c2d8410dd3df8
qwfsdaf <xogh990@gmail.com>
2024-03-14 11:16:15 +0900

    주석 삽입

--- 0314PJ/0314_5846394_1.c
+++ 0314PJ/0314_5846394_1.c
@@ -4,19 +4,19 @@
 
 int main(void) {
 	clock_t start, stop;
-	double duration;
-	int result = 0;
-	int k = 0;
-	start = clock();
-	for (int i = 2; i <= 100; i++) {
+	float duration;
+	int result = 0; //소수의 합 저장할 변수선언
+	int k = 0; 
+	start = clock(); //수행 시간측정 시작
+	for (int i = 1; i <= 100; i++) {
 		for (k = 2; k < i; k++) {
 			if (i % k == 0) break;
 		}
 		if (k == i)
 			result += k;
 	}
-	stop = clock();
-	duration = (double)(stop - start) / CLOCKS_PER_SEC;
+	stop = clock(); //수행 시간측정 종료
+	duration = (float)(stop - start) / CLOCKS_PER_SEC; //측정시간 구하기
 	printf("0부터 100까지의 소수의 합 : %d\n", result);
 	printf("수행시간은 %f초입니다.\n", duration);
 }
